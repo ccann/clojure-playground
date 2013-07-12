244b4645130628bae742cfbdc130ab0cc9b3b923
Cody Canning <cocanning11@gmail.com>
2013-07-12 17:45:10 -0400

    added fizzbuzz

--- playground/src/playground/core.clj
+++ playground/src/playground/core.clj
@@ -14,3 +14,25 @@
           (to-array (cons (char ascii-dash) (cons (char (+ ascii-zero rem)) acc)))
           (to-array (cons (char (+ ascii-zero rem)) acc)))
         (recur (/ num 10) (mod num 10) (cons (char (+ ascii-zero rem)) acc))))))
+
+
+;;FizzBuzz solution 1
+(defn mult5? [n]
+  (= 0 (mod n 5)))
+
+(defn mult3? [n]
+  (= 0 (mod n 3)))
+
+(defn fizz-buzz []
+  (for [x (map inc (range 100))]
+    (cond (and (mult3? x) (mult5? x)) "FizzBuzz"
+          (mult3? x) "Fizz"
+          (mult5? x) "Buzz"
+          :else x)))
+
+;;FizzBuzz solution 2
+(defn fizz-buzz2 []
+  (map #(cond (= (mod % 15) 0) "FizzBuzz"
+              (= (mod % 3) 0) "Fizz"
+              (= (mod % 5) 0) "Buzz"
+              :else %) (map inc (range 100))))
